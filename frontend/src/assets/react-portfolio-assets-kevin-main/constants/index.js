90ada972bcacddf259ce558b3b24aac97eed76e2
Khoa280703 <buidangkhoa2807@gmail.com>
2025-07-14 14:50:17 +0700

    update content

--- frontend/src/assets/react-portfolio-assets-kevin-main/constants/index.js
+++ frontend/src/assets/react-portfolio-assets-kevin-main/constants/index.js
@@ -1,7 +1,6 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import cholimex from "../assets/projects/edit-map.png";
+import ussh from "../assets/projects/ussh.jpg";
+import weafore from "../assets/projects/weafore.png";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -9,68 +8,47 @@ export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer w
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "July 2024 - June 2025",
+    role: "Full-stack Software Developer",
+    company: "BKCORETECH.",
+    description: `Developed a management system for the Vinh Loc Industrial Park, under the Cholimex group.`,
+    technologies: ["ReactJS", "NodeJS", "Postgres", "Redis", "AntDesign", "Socket.IO", "RabbitMQ", "Docker"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "May 2024 - July 2025",
+    role: "Mobile Developer Intern",
+    company: "BKCORETECH",
+    description: `Developed various features for mobile applications for universities with more than 10,000 users (USSH-VNHCM application)`,
+    technologies: ["Flutter", "NodeJS", "Oracle"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
-    image: project1,
+    title: "Vinh Loc industrial park's management system",
+    image: cholimex,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Human resource , departments, planning, projects, documents, electricity and water, .... management system of Vinh Loc industrial park belonging to Cholimex group",
+    technologies: ["ReactJS", "NodeJS", "Postgres", "Redis", "AntDesign", "Socket.IO", "RabbitMQ", "Docker"],
   },
   {
-    title: "Task Management App",
-    image: project2,
+    title: "USSH-VNHCM",
+    image: ussh,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Software application of University of Social Sciences and Humanities, Vietnam National University Ho Chi Minh City",
+    technologies: ["Flutter", "NodeJS", "Oracle"],
   },
   {
-    title: "Portfolio Website",
-    image: project3,
+    title: "Weafore",
+    image: weafore,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "Weather forecasting platform with sensors, automatic light off, remote canopy.",
+    technologies: ["ReactJS", "NodeJS", "MongoDB"],
   },
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Nguyen Van Luong, Binh Tien district, Ho Chi Minh city",
+  phoneNo: "+84 918 215 248",
+  email: "buidangkhoa2807@gmail.com",
 };
