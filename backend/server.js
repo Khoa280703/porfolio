ccb26702057ed2412aa3625ec3f67540c1179c17
Khoa280703 <buidangkhoa2807@gmail.com>
2025-07-21 17:25:19 +0700

    temp

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,12 @@
-// backend/server.js
 import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = 5001; // Chọn một port cho backend
+const PORT = 5001;
 
-// Sử dụng CORS middleware
 app.use(cors());
 
-// Tạo một API endpoint đơn giản
 app.get('/api/message', (req, res) => {
-    // Trả về một object JSON
     res.json({ message: 'Xin chào từ server Node.js! 👋' });
 });
 
@@ -18,7 +14,6 @@ app.get('/api/getList', (req, res) => {
     res.json({ list: ['Item 1', 'Item 2', 'Item 3'] });
 });
 
-// Lắng nghe ở port đã định nghĩa
 app.listen(PORT, () => {
     console.log(`🚀 Server backend đang chạy tại http://localhost:${PORT}`);
 });
